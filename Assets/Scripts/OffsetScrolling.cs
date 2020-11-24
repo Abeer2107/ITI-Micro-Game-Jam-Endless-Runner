f3d133b9f7d88dae6cb430cf348e0e3c67895a60
Abeer <abeerMojahed@gmail.com>
2020-11-24 19:17:43 +0200

    Cleaning up and replacing BG with scrolling material

---
+++ Assets/Scripts/OffsetScrolling.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class OffsetScrolling : MonoBehaviour
+{
+    [SerializeField] public float scrollSpeed = 0.1f;
+    private Renderer thisRenderer;
+
+    void Start()
+    {
+        thisRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        thisRenderer.sharedMaterial.SetTextureOffset("_MainTex", new Vector2(x, 0));
+    }
+}
